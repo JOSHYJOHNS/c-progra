cdf7ec7376518b76a4592a482963651ff490bfde
JOSHYJOHNS <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-02-07 14:56:48 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char str[100];
+   int i = 0,;
+   clrscr();
+   puts("Enter any string\n");
+   for(i = 0; str[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the string are %d\n", l);
+   for(i = 0; i <= l-1; i++)
+      {
+    f = f + 1;
+      }
+   }
+   printf("The number of words in the string are %d", f);
+   getch();
+}
