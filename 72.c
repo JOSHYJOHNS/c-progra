7a4c180784dca9a9c6434d4eff2ecd5ca9a21f34
Joshy Johns <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-03-05 13:32:28 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+    for(i=2; i<=n/2; ++i)
+    {
+    if(n%i==0)
+    {
+    if (flag==0)
+    printf("%d is not a prime number.",n); 
+    return 0;
+}
