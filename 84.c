2e94672327caef012240b4b83d803998369d1f09
Joshy Johns <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-03-09 15:51:12 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, rows;
+    printf("Enter number of rows: ");
+    scanf("%d",&rows);
+    
+    for(j=1; j<=i; ++j)
+    printf("* ");
+    printf("\n");
+    return 0;
+}
